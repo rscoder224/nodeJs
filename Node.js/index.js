69fcea777b1583a6fe47a91315e6517154deb2ab
Shaurya Tiwari <ltsmartofficial@gmail.com>
2025-04-03 16:16:24 +0530

    Add files via upload

--- Node.js/index.js
+++ Node.js/index.js
@@ -1,10 +1,13 @@
-const app = require("./app")
+const http = require('http')
+
+const data = require('./data');
+const { json } = require('stream/consumers');
 
 
-console.log(app['phone Number']);
-
-
-
-console.warn(app.a);
+http.createServer((req,res)=>{
+    res.writeHead({'Content-type': 'application\json'});
+    res.write(200,JSON.stringify(data));
+    res.end();
+}).listen(5000);
 
 
